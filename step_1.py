6005d1ce6d3b776c24566ca2edbb12eeeb997aeb
k.kuznetsov <k.kuznetsov@inlinegroup.ru>
2025-08-27 15:56:25 +0300

    2 new excs done

--- step_1.py
+++ step_1.py
@@ -1,7 +1,22 @@
+# # На автобусе
+# boy_1 = 'Вася'
+# boy_2 = 'Петя'
+# bus_number = 59
+#
+# print(f'{boy_1} и {boy_2} ездят в школу на {bus_number} автобусе.')
+#
+# # В школу
+# boy_1 = input()
+# boy_2 = input()
+# bus_number = input()
+#
+# print(f'{boy_1} и {boy_2} ездят в школу на {bus_number} автобусе.')
 
-# На автобусе
-boy_1 = input()
-boy_2 = input()
-bus_number = 59
+# Приглашение на праздник
+name = input()
+prazdnik = input()
+place = input()
 
-print(f'{boy_1} и {boy_2} ездят в школу на {bus_number} автобусе.')
+print(f'{name} !')
+print(f'Приглашаем отметить праздник {prazdnik} вместе с нами!')
+print(f'{place} - лучшее место для праздника!')
