9e079cd2c4449ece8bff3b5c25f706b8b76c23ed
k.kuznetsov <k.kuznetsov@inlinegroup.ru>
2025-08-28 17:21:33 +0300

    final step1

--- step_1.py
+++ step_1.py
@@ -13,10 +13,23 @@
 # print(f'{boy_1} и {boy_2} ездят в школу на {bus_number} автобусе.')
 
 # Приглашение на праздник
-name = input()
-prazdnik = input()
-place = input()
+# name = input()
+# prazdnik = input()
+# place = input()
+#
+# print(f'{name} !')
+# print(f'Приглашаем отметить праздник {prazdnik} вместе с нами!')
+# print(f'{place} - лучшее место для праздника!')
 
-print(f'{name} !')
-print(f'Приглашаем отметить праздник {prazdnik} вместе с нами!')
-print(f'{place} - лучшее место для праздника!')
+# Считалка с именами
+# name1, name2, name3, name4 = input(), input(), input(), input()
+# name5, name6, name7, name8 = input(), input(), input(), input()
+#
+# print(f'''Побежали на качели
+# {name1} , {name2} , {name3} , {name4} ,
+# Раскачали их на славу
+# {name5} , {name6} , {name7} .
+# Загорал один {name8} .
+# Лучше б он, как ты, водил!''')
+
+# Простой тест
