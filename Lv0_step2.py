3fc56483f3eadafb1a4b59b5dededa69d6c25288
k.kuznetsov <k.kuznetsov@inlinegroup.ru>
2025-09-01 17:23:31 +0300

    1-3 exrc.

--- Lv0_step2.py
+++ Lv0_step2.py
@@ -1,1 +1,20 @@
 # Преобразование типов данных
+# 1
+# a, b = int(input()), int(input())
+#
+# print(f'{a} + {b} = {a + b}')
+
+# 2 Объём куба
+# x = int(input())
+#
+# Skub = x ** 3
+# print(Skub)
+
+# 3 Всем поровну
+# p = int(input())
+# v = int(input())
+#
+# print(p // (v + 1))
+# print((p % (v + 1)))
+
+#4 Землекопы
