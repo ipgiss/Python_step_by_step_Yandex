90d6e1fd69308653c2e0cfc54324e5e7eb6f81e5
k.kuznetsov <k.kuznetsov@inlinegroup.ru>
2025-09-05 15:11:29 +0300

    math simple numbers

---
+++ math.py
@@ -0,0 +1,16 @@
+# Функция для проверки, является ли число простым
+
+n = int(input('Введите число: '))
+
+
+def is_prime(n):
+    if n < 2:
+        return False
+    for i in range(2, int(n ** 0.5) + 1):
+        if n % i == 0:
+            return False
+    return True
+
+
+print(is_prime(n))
+
