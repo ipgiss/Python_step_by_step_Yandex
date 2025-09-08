a74ed9f62d71ebdcecb345ab05413b0b23d5d260
k.kuznetsov <k.kuznetsov@inlinegroup.ru>
2025-09-08 11:14:52 +0300

    NOD-NOK

--- math.py
+++ math.py
@@ -1,16 +1,37 @@
 # Функция для проверки, является ли число простым
 
-n = int(input('Введите число: '))
+# n = int(input('Введите число: '))
+#
+#
+# def is_prime(n):
+#     if n < 2:
+#         return False
+#     for i in range(2, int(n ** 0.5) + 1):
+#         if n % i == 0:
+#             return False
+#     return True
+#
+#
+# print(is_prime(n))
+
+# Нахождения НОД и НОК двух чисел
+import math
+
+num1, num2 = int(input()), int(input())
+
+# nok = abs(num1 * num2) // math.gcd(num1, num2)
+#
+# print(f'НОД({num1},{num2}) = {math.gcd(num1, num2)}')
+# print(f'НОК({num1},{num2}) = {nok}')
+
+'''Вариант через вызов функции'''
 
 
-def is_prime(n):
-    if n < 2:
-        return False
-    for i in range(2, int(n ** 0.5) + 1):
-        if n % i == 0:
-            return False
-    return True
+def NOK(a, b):  # 1. Определение функции
+    return abs(a * b) // math.gcd(a, b)
 
 
-print(is_prime(n))
+result_nok = NOK(num1, num2)  # 2. Вызов функции с аргументами!
 
+print(f'НОД({num1}, {num2}) = {math.gcd(num1, num2)}')
+print(f'НОК({num1}, {num2}) = {result_nok}')
