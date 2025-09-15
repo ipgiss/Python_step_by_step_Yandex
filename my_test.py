43198b7b7d0355fef171496b10152a118962d7f1
k.kuznetsov <k.kuznetsov@inlinegroup.ru>
2025-09-15 12:35:25 +0300

    while True

--- my_test.py
+++ my_test.py
@@ -1,9 +1,9 @@
 while True:
     try:
-        numb1 = int(input('Введите число: '))
+        numb1 = int(input('Введите первое число: '))
         numb2 = int(input('Введите второе число: '))
-    except:
-        ValueError('Введите только число!! Попробуйте еще раз.')
+    except ValueError:
+        print('Введите только число!! Попробуйте еще раз.')
         continue  # вот это важно, а то цикл не закончится ))
 
     result = numb1 * numb2
